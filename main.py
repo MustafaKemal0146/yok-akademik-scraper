98db416aa54b10a43dd2740d738c1d25415565d0
MustafaKemal0146 <67921292+MustafaKemal0146@users.noreply.github.com>
2025-01-12 16:35:02 +0300

    main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from yok_scraper import YOKScraper
 import sys
 from colorama import init, Fore, Style
+from pyfiglet import Figlet
 
 # Colorama'yı başlat
 init(autoreset=True)
 
+def print_banner():
+    f = Figlet(font='big')
+    print(Fore.CYAN + "=" * 70)
+    print(Fore.RED + Style.BRIGHT + f.renderText('YOK WEB'))
+    print(Fore.GREEN + Style.BRIGHT + f.renderText('SCRAPER'))
+    print(Fore.CYAN + "=" * 70)
+    print(Fore.YELLOW + Style.BRIGHT + "\n" + " " * 20 + "YÖK Akademik Veri Çekme Aracı v1.3.0" + "\n")
+
 def main():
+    # Banner'ı göster
+    print_banner()
+    
     # Komut satırı argümanı verilmişse onu kullan, verilmemişse kullanıcıdan iste
     if len(sys.argv) == 2:
         url = sys.argv[1]
     else:
-        print(Fore.GREEN + Style.BRIGHT + "\nYÖK Akademik Veri Çekme Programı")
-        print(Fore.GREEN + Style.BRIGHT + "-" * 40)
         print(Fore.CYAN + "Örnek URL: https://akademik.yok.gov.tr/AkademikArama/view/searchResultviewAuthor.jsp?authorId=XXXX")
         url = input(Fore.YELLOW + "\nLütfen YÖK Akademik profil URL'sini girin: ")
     
